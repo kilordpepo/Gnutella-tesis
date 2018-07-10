24fa3bd53b7c512dccb275b0c1fe9186d77fbc3f
kilordpepo <kilordpepo@gmail.com>
2018-07-09 20:42:55 -0400

    arreglo

--- src/servant/Listener.java
+++ src/servant/Listener.java
@@ -1,25 +1,14 @@
 package servant;
 
-
-
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 
-
-
-
-
-
-
-
-
 //create client socket
 public class Listener implements Runnable {
 	private Socket clientSocket;
-
 
 	public void run() {
 		try {
@@ -28,61 +17,36 @@ public class Listener implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	
-	
-	
-	
-	
+
 	public void serverResponse() throws Exception {
-		
+
 		// New connection, check for the connection headers
-       
-			
-	
+
 		System.out.println("Listener.... OK");
 
 		int bufferSize = 8192;
 		while (true) {
-				try {
-					ServerSocket welcomeSocket = Server.mySocket;
-					
-					byte[] header = new byte[16];
-					Socket clientSocket = welcomeSocket.accept();
-					System.out.println("New connection accepted" + clientSocket.getInetAddress() );
-					this.clientSocket = clientSocket;
-					ByteBuffer bf = ByteBuffer.allocate(bufferSize);
-					BufferedInputStream inFromClient = new BufferedInputStream(clientSocket.getInputStream());
-					while (true) {
-					        int b = inFromClient.read();
-					        System.out.println(b);
-					        if (b == -1) {
-					            break;
-					        }
-					        bf.put( (byte) b);
-					    }
-					   // clientSocket.close();
-					}	
-				 catch (IOException e) {
-					System.out.println("Accept failed at " + this.clientSocket.getPort());
-					continue;
-				}
-				
+			try {
+				ServerSocket welcomeSocket = Server.mySocket;
 
-			
+				byte[] header = new byte[16];
+				Socket clientSocket = welcomeSocket.accept();
+				System.out.println("New connection accepted" + clientSocket.getInetAddress());
+				this.clientSocket = clientSocket;
+				ByteBuffer bf = ByteBuffer.allocate(bufferSize);
+				BufferedInputStream inFromClient = new BufferedInputStream(clientSocket.getInputStream());
+				inFromClient.read(header);
+				for (byte b : header)
+					bf.put((byte) b);
+
+				// clientSocket.close();
+			} catch (IOException e) {
+				System.out.println("Accept failed at " + this.clientSocket.getPort());
+				continue;
+			}
+
+		}
+
 	}
-		
-	}
-	
+
 }
-
-
-
-
-
-
-
-	
-	
-	
-	
-
